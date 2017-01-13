0f3b62116cd1bc267cbd1ed3c078fc938ddc7488
David Taylor <david.taylor@everylifetechnologies.com>
2017-01-13 21:54:24 +0000

    Added more UI StufF

---
+++ ios/lycan/src/ui/lobby/ConnectedPlayerTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ConnectedPlayerTableViewCell.swift
+//  lycan
+//
+//  Created by David Taylor on 13/01/2017.
+//  Copyright © 2017 TreeDaveGeorgeNeil. All rights reserved.
+//
+
+import UIKit
+
+class ConnectedPlayerTableViewCell: UICollectionViewCell {
+    
+    @IBOutlet var readyImageView: UIImageView!
+}
