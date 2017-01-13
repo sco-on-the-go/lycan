c9f11e3e89ab2bcf38418354cf838681cf16e92e
George Smith <gws@compsoft.com>
2017-01-13 22:03:47 +0000

    Post merge conflict changes soz

--- ios/lycan/src/Networking/Response/JoinGameResponse.swift
+++ ios/lycan/src/Networking/Response/JoinGameResponse.swift
@@ -1,14 +1,14 @@
 //
-//  JoinGameRequest.swift
+//  JoinGameResponse.swift
 //  lycan
 //
 //  Created by George Smith on 13/01/2017.
 //  Copyright © 2017 TreeDaveGeorgeNeil. All rights reserved.
 //
-
 import Foundation
 
 
 class JoinGameResponse {
     var name: String!
+    
 }
