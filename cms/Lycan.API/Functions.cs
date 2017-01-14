0dec2a0c2006f05e09136b193202e6d53cc26feb
Neil Bostrom <neil@ipresent.com>
2017-01-14 00:28:20 +0000

    Completed IsReady

--- cms/Lycan.API/Functions.cs
+++ cms/Lycan.API/Functions.cs
@@ -68,17 +68,29 @@ namespace Lycan.API
         {
             var requestBody = JsonConvert.DeserializeObject<HostGameRequest>(request?.Body);
 
-            Game game = new Game() { GameId = Guid.NewGuid(), Name = requestBody.Name, State = GameStateEnum.Lobby };
+            Game game = new Game() { GameId = Guid.NewGuid(), Name = requestBody.GameName, State = GameStateEnum.Lobby };
 
-            context.Logger.LogLine($"{requestBody.Name} has been created as a new game");
+            context.Logger.LogLine($"{requestBody.GameName} has been created as a new game");
             await DDBContext.SaveAsync(game);
+            
+            // Add the three table down cards
+            Player card1 = new Player() { PlayerId = Guid.NewGuid(), GameId = game.GameId, Name = "Table Down 1", IsReady = true, IsNPC = true };
+            await DDBContext.SaveAsync(card1);
+            Player card2 = new Player() { PlayerId = Guid.NewGuid(), GameId = game.GameId, Name = "Table Down 2", IsReady = true, IsNPC = true };
+            await DDBContext.SaveAsync(card2);
+            Player card3 = new Player() { PlayerId = Guid.NewGuid(), GameId = game.GameId, Name = "Table Down 3", IsReady = true, IsNPC = true };
+            await DDBContext.SaveAsync(card3);
+
+            Player player = new Player() { PlayerId = Guid.NewGuid(), GameId = game.GameId, Name = requestBody.PlayerName, IsReady = false };
+            await DDBContext.SaveAsync(player);
 
             return new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
                 Body = JsonConvert.SerializeObject(new HostGameResponse()
                 {
-                    GameId = game.GameId
+                    GameId = game.GameId,
+                    PlayerId = player.PlayerId
                 }),
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
@@ -96,7 +108,7 @@ namespace Lycan.API
 
             var game = games.FirstOrDefault();
 
-            Player player = new Player() { PlayerId = Guid.NewGuid(), GameId = game.GameId, Name = requestBody.PlayerName, IsReady = false };
+            Player player = new Player() { PlayerId = Guid.NewGuid(), GameId = game.GameId, Name = requestBody.PlayerName, IsReady = false, IsNPC = false };
 
             context.Logger.LogLine($"{requestBody.PlayerName} joined the game {game.GameId}");
             await DDBContext.SaveAsync(player);
@@ -135,8 +147,33 @@ namespace Lycan.API
 
             if (players.All(p => p.IsReady == true))
             {
+                List<PlayerTypeEnum> types = new List<API.PlayerTypeEnum>()
+                {
+                    PlayerTypeEnum.Werewolf,
+                    PlayerTypeEnum.Werewolf,
+                    PlayerTypeEnum.Robber,
+                    PlayerTypeEnum.Troublemaker,
+                    PlayerTypeEnum.Seer
+                };
+                
+                foreach (Player gamePlayer in players.OrderBy(x => Guid.NewGuid()))
+                {
+                    if (types.Any())
+                    {
+                        gamePlayer.PlayerType = types[0];
+                        types.RemoveAt(0);
+                    }
+                    else
+                        gamePlayer.PlayerType = PlayerTypeEnum.Villager;
+
+                    if (player.PlayerId == gamePlayer.PlayerId)
+                        player.PlayerType = gamePlayer.PlayerType;
+
+                    await DDBContext.SaveAsync(gamePlayer);
+                }
+
                 game.State = GameStateEnum.Ready;
-                await DDBContext.SaveAsync(player);
+                await DDBContext.SaveAsync(game);
             }
 
             return new APIGatewayProxyResponse
@@ -144,8 +181,42 @@ namespace Lycan.API
                 StatusCode = (int)HttpStatusCode.OK,
                 Body = JsonConvert.SerializeObject(new IsReadyResponse()
                 {
-                    Players = players.Select(p => new IsReadyResponse.PlayerViewModel { PlayerId = p.PlayerId, Name = p.Name, IsReady = p.IsReady }).ToList(),
-                    GameState = game.State
+                    Players = players.Select(p => new IsReadyResponse.PlayerViewModel { PlayerId = p.PlayerId, Name = p.Name, IsReady = p.IsReady, PlayerType = p.PlayerType, IsNPC = p.IsNPC }).ToList(),
+                    GameState = game.State,
+                    PlayerType = player.PlayerType
+                }),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+
+        public async Task<APIGatewayProxyResponse> VoteAsync(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var requestBody = JsonConvert.DeserializeObject<IsReadyRequest>(request?.Body);
+
+            Player player = await DDBContext.LoadAsync<Player>(requestBody.PlayerId);
+            if (player == null)
+                throw new Exception($"{requestBody.PlayerId} was not found");
+
+            player.IsReady = true;
+            await DDBContext.SaveAsync(player);
+
+            var scanResult = DDBContext.ScanAsync<Player>(new[] { new ScanCondition("GameId", Amazon.DynamoDBv2.DocumentModel.ScanOperator.Equal, player.GameId) });
+            var players = await scanResult.GetRemainingAsync();
+
+            context.Logger.LogLine($"{players.Count(p => p.IsReady)} Ready : {players.Count(p => !p.IsReady)} Not Ready");
+
+            Game game = await DDBContext.LoadAsync<Game>(player.GameId);
+            if (game == null)
+                throw new Exception($"{player.GameId} was not found");
+            
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = JsonConvert.SerializeObject(new IsReadyResponse()
+                {
+                    Players = players.Select(p => new IsReadyResponse.PlayerViewModel { PlayerId = p.PlayerId, Name = p.Name, IsReady = p.IsReady, PlayerType = p.PlayerType, IsNPC = p.IsNPC }).ToList(),
+                    GameState = game.State,
+                    PlayerType = player.PlayerType
                 }),
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
@@ -153,12 +224,14 @@ namespace Lycan.API
 
         public class HostGameRequest
         {
-            public string Name { get; set; }
+            public string GameName { get; set; }
+            public string PlayerName { get; set; }
         }
 
         public class HostGameResponse
         {
             public Guid GameId { get; set; }
+            public Guid PlayerId { get; set; }
         }
 
         public class JoinGameRequest
@@ -182,12 +255,15 @@ namespace Lycan.API
         {
             public List<PlayerViewModel> Players { get; set; }
             public GameStateEnum GameState { get; set; }
+            public PlayerTypeEnum PlayerType { get; set; }
 
             public class PlayerViewModel
             {
                 public Guid PlayerId { get; set; }
                 public string Name { get; set; }
                 public bool IsReady { get; set; }
+                public bool IsNPC { get; set; }
+                public PlayerTypeEnum PlayerType { get; set; }
             }
         }
 
--- cms/Lycan.API/Tables/Player.cs
+++ cms/Lycan.API/Tables/Player.cs
@@ -9,13 +9,23 @@ using Amazon.DynamoDBv2.DataModel;
 
 namespace Lycan.API
 {
+    public enum PlayerTypeEnum
+    {
+        Werewolf = 1,
+        Seer,
+        Troublemaker,
+        Robber,
+        Villager
+    }
+
     public class Player
     {
         [DynamoDBHashKey]
         public Guid PlayerId { get; set; }
-        [DynamoDBRangeKey]
         public Guid GameId { get; set; }
         public string Name { get; set; }
         public bool IsReady { get; set; }
+        public bool IsNPC { get; set; }
+        public PlayerTypeEnum PlayerType { get; set; }
     }
 }
